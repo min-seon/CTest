62af8515ea4b573f826a2272285f1702458b754f
minseon <titanic1997@ajou.ac.kr>
2022-09-16 22:59:41 +0900

    codint test book

---
+++ 020601_4.py
@@ -0,0 +1,15 @@
+#계수 정렬
+
+array=[7,5,9,0,3,1,6,2,9,1,4,8,0,5,2]
+
+count=[0] * (max(array)+1)
+
+for i in array:
+    count[i] +=1
+
+print(count)
+
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end =' ')
---
+++ 020601_5.py
@@ -0,0 +1,3 @@
+array = [('바나나',1),('사과',2),('포도',3)]
+key=sorted(array)
+print(key)
